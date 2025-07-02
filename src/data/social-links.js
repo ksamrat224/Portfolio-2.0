800eadc5e100cd9220530b6b0efa144af536f61b
ksamrat224 <ksamrat224@gmail.com>
2025-07-02 19:41:39 +0545

    constants

---
+++ src/data/social-links.js
@@ -0,0 +1,26 @@
+export const socialLinks = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/ksamrat224',
+    icon: 'Github',
+    color: 'hover:text-gray-800 dark:hover:text-gray-200'
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://linkedin.com/in/samratkarki',
+    icon: 'Linkedin',
+    color: 'hover:text-blue-600'
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/samratkarki',
+    icon: 'Twitter',
+    color: 'hover:text-blue-400'
+  },
+  {
+    name: 'Email',
+    url: 'mailto:samrat.karki@example.com',
+    icon: 'Mail',
+    color: 'hover:text-green-600'
+  }
+]
